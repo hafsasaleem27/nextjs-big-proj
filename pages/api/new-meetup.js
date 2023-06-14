0a59ac2af9b4f911a3358541f6c1370fda1e57f6
Hafsa Saleem <hafsa.saleem27@gmail.com>
2023-06-14 21:01:50 +0500

    conneccted to mongodb and sent request to /api/new-meetup

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -7,7 +7,7 @@ async function handler(req, res) {
 
     const { title, image, address, description } = data;
     const client = await MongoClient.connect(
-      "mongodb+srv://hafsa:AWNUwG6Sv9p4Ol9N@cluster0.8uf0uos.mongodb.net/?retryWrites=true&w=majority"
+      "mongodb+srv://hafsa:8ThDKkQ6jm7w8Y3C@cluster0.8uf0uos.mongodb.net/?retryWrites=true&w=majority"
     );
 
     const db = client.db();
