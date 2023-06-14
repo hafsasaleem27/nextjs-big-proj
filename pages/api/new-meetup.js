6bd4c3bc19e13c747832bfefe5202fb177847cec
Hafsa Saleem <hafsa.saleem27@gmail.com>
2023-06-14 17:54:45 +0500

    added meetup to db and sent back response

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,10 +1,22 @@
 // POST /api/new-meetup
+import { MongoClient } from "mongodb";
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
     const { title, image, address, description } = data;
+    const client = await MongoClient.connect(
+      "mongodb+srv://hafsa:AWNUwG6Sv9p4Ol9N@cluster0.8uf0uos.mongodb.net/?retryWrites=true&w=majority"
+    );
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+    client.close();
+    res.status(201).json({ message: "Meetup Inserted!" });
   }
 }
 
