f36983535d8ed4258095b071a36a7667a7a04489
Hafsa Saleem <hafsa.saleem27@gmail.com>
2023-06-19 18:32:37 +0500

    added meta tag to the meetups page

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,19 @@
-import { useEffect, useState } from "react";
+import { Fragment } from "react";
+import Head from "next/head";
 import { MongoClient } from "mongodb";
 import MeetupList from "../components/meetups/MeetupList";
 
-const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    title: "A first meetup",
-    image: "",
-    address: "Some address 5, 12345 Some City",
-    description: "This is a first meetup",
-  },
-  {
-    id: "m2",
-    title: "A second meetup",
-    image: "",
-    address: "Some address 10, 12345 Some City",
-    description: "This is a second meetup",
-  },
-];
-
 function HomePage(props) {
-  // const [loadedMeetups, setLoadedMeetups] = useState([]);
-
-  // useEffect(() => {
-  //   // send an http request to fetch data
-  //   setLoadedMeetups(DUMMY_MEETUPS);
-  // }, []);
-
-  return <MeetupList meetups={props.meetups} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>React Meetups</title>
+        <meta name="description" content="A list of meetups"/>
+      </Head>
+      <MeetupList meetups={props.meetups} />
+    </Fragment>
+  );
 }
-
-// export async function getServerSideProps(context) {
-//   const req = context.req;
-//   const res = context.res;
-
-//   // fetch data from an API
-  
-// }
 
 export async function getStaticProps() {
   // fetch data from an API
@@ -53,7 +30,7 @@ export async function getStaticProps() {
 
   return {
     props: {
-      meetups: meetups.map(meetup => ({
+      meetups: meetups.map((meetup) => ({
         title: meetup.title,
         image: meetup.image,
         description: meetup.description,
